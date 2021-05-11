e02d25a9270ce747b7195253710227e174cd4318
Lucian Cochior <lucian.cochiorheghi@gmail.com>
2021-05-11 21:25:22 +0300

    refactor filterCarModels functions

--- src/main/java/org/cleancode/SearchEngine.java
+++ src/main/java/org/cleancode/SearchEngine.java
@@ -1,20 +1,39 @@
 package org.cleancode;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SearchEngine {
-    public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> carModels) {
+    public List<CarModel> filterCarModels2(CarSearchCriteria criteria, List<CarModel> carModels) {
         Iterator<CarModel> carModelIterator = carModels.listIterator();
         while (carModelIterator.hasNext()) {
             CarModel currentCarModel = carModelIterator.next();
-            YearInterval carYearInterval = new YearInterval(criteria.getYearInterval().getStart(), criteria.getYearInterval().getEnd());
-            YearInterval criteriaYearInterval = new YearInterval(currentCarModel.getYearInterval().getStart(), currentCarModel.getYearInterval().getEnd());
+            YearInterval carYearInterval = currentCarModel.getYearInterval();
+            YearInterval criteriaYearInterval = criteria.getYearInterval();
             if (!carYearInterval.intersect(criteriaYearInterval)) {
                 carModelIterator.remove();
             }
         }
         return carModels;
+    }
+
+    public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> carModels) {
+        List<CarModel> filteredCarModels = new ArrayList<>();
+        for (CarModel carModel : carModels) {
+            if (carModel.getYearInterval().intersect(criteria.getYearInterval())) {
+                filteredCarModels.add(carModel);
+            }
+        }
+
+        return filteredCarModels;
+    }
+
+    public List<CarModel> filterCarModels3(CarSearchCriteria criteria, List<CarModel> carModels) {
+        return carModels.stream()
+                .filter(carModel -> carModel.getYearInterval().intersect(criteria.getYearInterval()))
+                .collect(Collectors.toList());
     }
 }
 
